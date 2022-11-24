0e156cb7580d94aeb1abd6e01cc9f8a3167c54dd
katibur <katiburrahmansany@gmail.com>
2022-11-24 21:33:22 +0600

    jwt category backend done

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -10,10 +13,91 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.aqlapfl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+async function run() {
+    try {
+        const usersCollection = client.db('wot').collection('users');
+        const categoriesCollections = client.db('wot').collection('categories');
+        const productsCollections = client.db('wot').collection('products');
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '24h' })
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ accessToken: '' })
+        });
+
+
+
+
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const categories = await categoriesCollections.find(query).toArray();
+            res.send(categories);
+        });
+
+
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { category_id: id }
+            const result = await productsCollections.find(filter).toArray();
+            res.send(result);
+        })
+
+
+
+
+
+
+        app.get('/users', async (req, res) => {
+            const query = {}
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
+
+
 app.get('/', async (req, res) => {
-    res.send('Wheels on tars is running');
+    res.send('wheels on tars is running');
 })
 
 app.listen(port, () => {
-    console.log(`Wheels on tars  running on port: ${port}`)
+    console.log(`wheels on tars running on port: ${port}`)
 })
